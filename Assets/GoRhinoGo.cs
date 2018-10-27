4e40c8420558121d6c05d248deb9e51bcfc3e9f6
aswartzell <aswartzell@gmail.com>
2018-10-27 14:51:47 -0400

    GoRhinoGo Menu

---
+++ Assets/GoRhinoGo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MenuItems
+{
+    [MenuItem("GoRhinoGo/Launch Grasshopper")]
+    private static void NewMenuOption()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
